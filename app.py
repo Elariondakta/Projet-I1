53dcc5b1741214f121d154051425d7b3332812d7
Elariondakta <prevottheodore@gmail.com>
2020-03-11 19:08:03 +0100

    examples

---
+++ app.py
@@ -0,0 +1,5 @@
+from npyscreen import NPSAppManaged
+
+class App(NPSAppManaged):
+    def __init__(self):
+        NPSAppManaged.addForm("starting",)
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import npyscreen
 
 
+
+if __name__ == '__main__':
+    App = App()
+    App.run()
