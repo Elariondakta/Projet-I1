02fcbc85deee49de9c76c1eab4a2aea368558154
Elariondakta <prevottheodore@gmail.com>
2020-03-19 17:06:39 +0100

    bug correction : les ids étaient pas uniques

--- generate.py
+++ generate.py
@@ -8,14 +8,14 @@ with open("./data/choice.json", "r") as f:
     
 for i in range(1, 4):
     for j in range(1, 20):
-        data["rooms"][str(uuid.uuid1())] = {
+        data["rooms"][str(uuid.uuid4())] = {
             "building_name": "NDL",
             "room_name": "L" + str(i) + str(j).zfill(2) #Pour avoir le format suivant : 01, 02, 03
         }
 
 for i in range(1, 5):
     for j in range(1, 9):
-        data["rooms"][str(uuid.uuid1())] = {
+        data["rooms"][str(uuid.uuid4())] = {
             "building_name": "NDC",
             "room_name": "N" + str(i) + str(j)
         }
