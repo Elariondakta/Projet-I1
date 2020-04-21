12c680b973bf2bf7928046865ec7e6d2784c97a2
Elariondakta <prevottheodore@gmail.com>
2020-04-21 17:47:19 +0200

    bug correction API

--- api.py
+++ api.py
@@ -10,131 +10,152 @@ class API():
 
     room_file = "./data/rooms.json"
 
-    def __init__(self):
-        self.__loadData()
+    @staticmethod 
+    def loadData():
+        with open(API.choice_file, 'r') as f:
+            API.choice_data = json.load(f)
+
+        with open(API.computer_file, 'r') as f:
+            API.computer_data = json.load(f)
         
+        with open(API.room_file, 'r') as f:
+            API.room_data = json.load(f)
 
-    def __loadData(self):
-        with open(self.choice_file, 'r') as f:
-            self.choice_data = json.load(f)
-
-        with open(self.computer_file, 'r') as f:
-            self.computer_data = json.load(f)
-        
-        with open(self.room_file, 'r') as f:
-            self.room_data = json.load(f)
-
-    def saveData(self):
+    @staticmethod 
+    def saveData():
         try:
-            with open(self.computer_file, 'w') as f:
-                json.dump(self.computer_data, f)
+            with open(API.computer_file, 'w') as f:
+                json.dump(API.computer_data, f)
 
-            with open(self.room_file, 'w') as f:
-                json.dump(self.room_data, f)
+            with open(API.room_file, 'w') as f:
+                json.dump(API.room_data, f)
         except BaseException:
             return False
         return True
 
     # ROOM
-    def addRoom(self, room, building):
-        self.room_data[str(uuid.uuid4())] = {
+    @staticmethod 
+    def addRoom(room, building):
+        API.room_data[str(uuid.uuid4())] = {
             "building_name": building,
             "room_name": room
         }
-        return self.saveData()
+        return API.saveData()
 
     """
     Renomme une salle avec commme arguments un id et le nouveau nom
     """
-    def renameRoom(self, id, name):
-        self.room_data[id]["room_name"] = name
-        return self.saveData()
+    @staticmethod 
+    def renameRoom(id, name):
+        API.room_data[id]["room_name"] = name
+        return API.saveData()
  
-    def removeRoom(self, id):
-        del self.room_data[id]
-        return self.saveData()
+    @staticmethod 
+    def removeRoom(id):
+        del API.room_data[id]
+        return API.saveData()
 
-    def getRooms(self):
-        return self.room_data
+    @staticmethod 
+    def getRooms():
+        return API.room_data
 
-    def getRoom(self, id):
-        return self.room_data[id]
+    @staticmethod 
+    def getRoom(id):
+        return API.room_data[id]
     """
     Retourne une liste de tous les bâtiments disponibles
     """
-    def getBuildings(self):
+    @staticmethod 
+    def getBuildings():
         results = {}
-        for room_id in self.room_data.keys():
-            if self.room_data[room_id]["building_name"] in results.values():
-                results[room_id] = self.room_data[room_id]["building_name"]
+        for room_id in API.room_data.keys():
+            if API.room_data[room_id]["building_name"] in results.values():
+                results[room_id] = API.room_data[room_id]["building_name"]
         return results
 
     """
     Recherche d'une salle parmi la liste de toutes les salles disponibles
     """
-    def searchRooms(self, query):
+    @staticmethod 
+    def searchRooms(query):
         results = {}
-        for room_id in self.room_data.keys():
-            if self.room_data[room_id]["room_name"].find(query) != -1:
-                results[room_id] = self.room_data[room_id]
+        for room_id in API.room_data.keys():
+            if API.room_data[room_id]["room_name"].find(query) != -1:
+                results[room_id] = API.room_data[room_id]
         return results
 
     #COMPUTER
-    def addComputer(self, computer):
-        self.computer_data[str(uuid.uuid4())] = computer
+    @staticmethod 
+    def addComputer(computer):
+        API.computer_data[str(uuid.uuid4())] = computer
 
-    def setComputer(self, id, computer):
-        self.computer_data[id] = computer
+    @staticmethod 
+    def setComputer(id, computer):
+        API.computer_data[id] = computer
 
-    def removeComputer(self, id):
-        del self.computer_data[id]
-        return self.saveData()
+    @staticmethod 
+    def removeComputer(id):
+        del API.computer_data[id]
+        return API.saveData()
 
-    def getComputers(self):
-        return self.computer_data
+    @staticmethod 
+    def getComputers():
+        return API.computer_data
 
-    def getComputer(self, id):
-        return self.computer_data[id]
+    @staticmethod 
+    def getComputer(id):
+        return API.computer_data[id]
 
     """
     Permet de trouver tous les ordinateurs dans une salle
     Renvoie un Dictionnaire avec les infos pour chaque ordinateur et son id comme clef
     """ 
-    def getComputersInRoom(self, roomId):
+    @staticmethod 
+    def getComputersInRoom(roomId):
         results = {}
-        for computer_key in list(self.computer_data.keys()):
-            if self.computer_data[computer_key]["localisation"]["room_id"] == roomId:
-                results[computer_key] = self.computer_data[computer_key]
+        for computer_key in list(API.computer_data.keys()):
+            if API.computer_data[computer_key]["localisation"]["room_id"] == roomId:
+                results[computer_key] = API.computer_data[computer_key]
         return results
 
     #SOFTWARE
-    def addSoftware(self, computer_id, software):
-        self.computer_data[computer_id]['softwares'][str(uuid.uuid4())] = software
+    @staticmethod 
+    def addSoftware(computer_id, software):
+        API.computer_data[computer_id]['softwares'][str(uuid.uuid4())] = software
 
-    def setSoftware(self, computer_id, software_id, software):
-        self.computer_data[computer_id]['softwares'][software_id] = software
+    @staticmethod 
+    def setSoftware(computer_id, software_id, software):
+        API.computer_data[computer_id]['softwares'][software_id] = software
 
-    def removeSoftware(self, computer_id, software_id):
-        self.computer_data[computer_id]['softwares'].remove(software_id)
+    @staticmethod 
+    def removeSoftware(computer_id, software_id):
+        API.computer_data[computer_id]['softwares'].remove(software_id)
 
-    def getSoftwares(self, computer_id):
-        return self.computer_data[computer_id]['softwares']
+    @staticmethod 
+    def getSoftwares(computer_id):
+        return API.computer_data[computer_id]['softwares']
 
-    def getSoftware(self, computer_id, software_id):
-        return self.computer_data[computer_id]['softwares'][software_id]
+    @staticmethod 
+    def getSoftware(computer_id, software_id):
+        return API.computer_data[computer_id]['softwares'][software_id]
 
     #PLUGINS
-    def addPlugin(self, computer_id, software_id, plugin):
-        self.computer_data[computer_id]['softwares'][software_id]['add_on'][str(uuid.uuid4())] = plugin
+    @staticmethod 
+    def addPlugin(computer_id, software_id, plugin):
+        API.computer_data[computer_id]['softwares'][software_id]['add_on'][str(uuid.uuid4())] = plugin
 
-    def setPlugin(self, computer_id, software_id, plugin_id, plugin):
-        self.computer_data[computer_id]['softwares'][software_id]['add_on'][plugin_id] = plugin
+    @staticmethod 
+    def setPlugin(computer_id, software_id, plugin_id, plugin):
+        API.computer_data[computer_id]['softwares'][software_id]['add_on'][plugin_id] = plugin
 
-    def removePlugin(self, computer_id, software_id, plugin_id):
-        self.computer_data[computer_id]['softwares'][software_id]['add_on'].remove(plugin_id)
+    @staticmethod 
+    def removePlugin(computer_id, software_id, plugin_id):
+        API.computer_data[computer_id]['softwares'][software_id]['add_on'].remove(plugin_id)
 
-    def getPlugins(self, computer_id, software_id):
-        return self.computer_data[computer_id]['softwares'][software_id]['add-on']
+    @staticmethod 
+    def getPlugins(computer_id, software_id):
+        return API.computer_data[computer_id]['softwares'][software_id]['add-on']
 
-    def getPlugin(self, computer_id, software_id, plugin_id):
-        return self.computer_data[computer_id]['softwares'][software_id]['add-on'][plugin_id]
+    @staticmethod 
+    def getPlugin(computer_id, software_id, plugin_id):
+        return API.computer_data[computer_id]['softwares'][software_id]['add-on'][plugin_id]
