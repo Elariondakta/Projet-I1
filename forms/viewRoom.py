3fa948286508a86935697472d22effa81772d508
clementdelestre <clementdelestre@gmail.com>
2020-04-22 19:22:24 +0200

    room

--- forms/viewRoom.py
+++ forms/viewRoom.py
@@ -4,12 +4,24 @@ import curses
 from utils import *
     
 class ViewRoom(npyscreen.ActionFormMinimal):
-    api = API()
+    isCreated = False
         
     def create(self):
-        self.build()
+        pass
 
     def build(self):
-        print(transfertArgs.args['room_id'])
+        if transfertArgs.args['room_id'] == "" or ViewRoom.isCreated:
+            return
+
+        self.roomField = self.add(npyscreen.FixedText, name="Salle", value = "Salle : " + API.getRoom(transfertArgs.args['room_id'])['room_name'], relx=10, )
+        self.roomField = self.add(npyscreen.FixedText, name="Batiment", value="Batiment : " + API.getRoom(transfertArgs.args['room_id'])['building_name'], relx=10, )
+
+    def while_editing(self, *args, **keywords):
+
+        self.build()
+        ViewRoom.isCreated = True
+        return super().while_editing(*args, **keywords)
+
+    
         
 
