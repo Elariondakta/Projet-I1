7b02427fdcb58c950e8f9a2dfea31ba7edcc88ae
ValentinRouge <rougevalentin@outlook.com>
2020-04-22 20:03:35 +0200

    add software

--- forms/addSoftware.py
+++ forms/addSoftware.py
@@ -1,5 +1,7 @@
 import npyscreen
 import curses
+from api import API
+
 #hiskfsdzzdzz
 #ssjdsjdsdsj
 #"software": {
@@ -15,14 +17,48 @@ import curses
 #                "editor": "Francis",
 #                "provider": "Microsoft",
 #                "version": "2.4.3892"
-# jzjz dz
-#jdjzjdd
-#dhd
+class ButtonAddAddOn(npyscreen.ButtonPress):
+    def __init__(self, *args, **keywords):
+        super(ButtonAddAddOn, self).__init__(*args, **keywords)
+        self.name="+ plug-in"
+        self.form_instance = keywords.get("form_instance")
 
+    def whenPressed(self):
+        self.form_instance.build()
 
+class ButtonAddAddOnBox(npyscreen.BoxTitle):
+    _contained_widget = ButtonAddAddOn
 
-class addSoftware(npyscreen.Form):      
-    def create(self):  
-        self.add(npyscreen.TitleText, value="Titre", relx=10)
-        self.add(npyscreen.TitleText,  value="Champ", relx=10)
-        self.add(npyscreen.ComboBox, value= " coucou", display_value="text") 
+class addSoftware(npyscreen.ActionFormV2):
+    def create(self):
+        self.build()
+
+    def build(self):
+        self.nameField = self.add(npyscreen.TitleText, name="Nom : ", relx=10)
+        self.nextrely += 1
+        self.editorField = self.add(npyscreen.TitleText, name="Editeur : ", relx=10)
+        self.nextrely += 1
+        self.providorField = self.add(npyscreen.TitleText, name="Fournisseur : ", relx=10)
+        self.nextrely += 1
+        self.versionField = self.add(npyscreen.TitleText, name="Version : ", relx=10)
+        self.nextrely += 1
+        self.license_expiration_dateField = self.add(npyscreen.TitleText, name="Date d'expiration de la license (JJ/MM/AAAA): ", relx=10)
+        self.nextrely += 1
+        self.IDField = self.add(npyscreen.TitleText, name="Identifiant : ", relx=10)
+        self.nextrely += 1
+        self.add(ButtonAddAddOnBox, form_instance = self)
+    
+    def on_ok(self):
+        if len(self.editorField.get_value()) == 0 or len(self.nameField.get_value()) == 0 or len(self.providorField.get_value()) == 0 or len(self.versionField.get_value()) == 0 or len(self.versionField.get_value()) == 0 or len(self.license_expiration_dateField.get_value()) == 0 or len(self.IDField.get_value()) == 0:
+            npyscreen.notify_confirm("Vous devez remplir tous les champs du formulaires avant de confirmer", "Erreur")
+            return;
+        
+        if API.addRoom(self.nameField.get_value(), self.nameField.get_value()):
+            npyscreen.notify_confirm("Le logiciel " + self.nameField.get_value() + " à bien été ajoutée", "Succès")
+            # RoomsForm.updateSearchResults(API.searchRooms(""))  #On réinitialise les donnée affichée pour que notre ajout soit affiché
+            self.find_parent_app().switchForm("SOFTWARE")
+        else:
+            npyscreen.notify_confirm("Une erreur est apparue lors de l'ajout de votre salle", "Erreur")
+
+    def on_cancel(self):
+        self.find_parent_app().switchForm("SOFTWARE")
