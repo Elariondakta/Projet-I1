5e7a8ac2f7aed265a57e3d659d303abab4d5d223
Elariondakta <prevottheodore@gmail.com>
2020-05-08 17:31:48 +0200

    REMOOOOOOOOVE

--- utils.py
+++
@@ -1,7 +0,0 @@
-import platform, os
-
-def clear():
-    if platform.system() == "Windows": ##Dans les cas ou on est sur Windaube 
-        os.system("cls")
-    else:   ##Dans le cas ou on est sur Mac os ou Linux
-        os.system("clear")
